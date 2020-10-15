c09ea0b51bb81f7105541d4610d97795b504fd6b
Danny <danisnotonfire.chen@mail.utoronto.ca>
2020-10-15 12:57:49 -0400

    Added methods to call on clicking the buttons.

--- events.js
+++ events.js
@@ -2,5 +2,14 @@ new Vue({
     el: '#vue-app',
     data: {
         age: 25
+    },
+    methods: {
+        add: function (increment) {
+            this.age += increment;
+        },
+
+        subtract: function (decrement) {
+            this.age -= decrement;
+        }
     }
 });
