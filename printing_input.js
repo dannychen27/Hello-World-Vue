d81e11dd85855655f92e8b8eb31476079f1b99ed
Danny <danisnotonfire.chen@mail.utoronto.ca>
2020-10-15 12:36:12 -0400

    Created a separate file for textboxes and links.

--- printing_input.js
+++ printing_input.js
@@ -5,7 +5,8 @@ new Vue({
     data: {
         name: 'Danny',
         job: 'Software Artist',
-        website: "https://www.google.com/"
+        // website: "https://www.google.com/",
+        websiteTag: '<a href="https://www.google.com/">Google Website</a>'
     },
     methods: {
         // output a greeting to the DOM
---
+++ user_input.js
@@ -0,0 +1,7 @@
+new Vue({
+    el: '#vue-app',
+    data: {
+        website: "https://www.google.com/",
+        websiteTag: '<a href="https://www.google.com/">Google Website</a>'
+    }
+});
