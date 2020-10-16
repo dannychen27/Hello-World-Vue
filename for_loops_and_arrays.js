488e584172ccf8a5103009f3dbce26a27ca26df8
Danny <danisnotonfire.chen@mail.utoronto.ca>
2020-10-16 00:45:50 -0400

    Added for loops to iterate through objects to Vue.

---
+++ for_loops_and_arrays.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: "#vue-app",
+    data: {
+        characters: ["Mario", "Luigi", "Yoshi", "Bowser"],
+        ninjas: [
+            { name: "A", age: 1 },
+            { name: "B", age: 2 },
+            { name: "C", age: 3 }
+        ]
+    },
+    methods: {},
+    computed: {}
+});
